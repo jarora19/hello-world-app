aa2499b623647814a3b4bf9fa0e3eb21d9048659
jarora19 <arora69.bunny@gmail.com>
2024-05-16 11:52:17 +0530

    Adding app.js file

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
