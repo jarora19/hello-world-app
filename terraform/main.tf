fe1d69a690f4371c198b782ea0ca0fb8152864ef
Jatin Arora <83803102+jarora19@users.noreply.github.com>
2024-06-07 15:59:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,7 +83,7 @@ resource "aws_lb" "my_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.ecs.id]
-  subnets            = aws_subnet.public.id
+  subnets            = aws_subnet.public[*].id
 }
 
 resource "aws_lb_target_group" "my_target_group" {
