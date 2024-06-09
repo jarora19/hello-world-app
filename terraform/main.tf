f087f1e2148a8918e6af963a0f1e9274faaecba6
jarora19 <arora69.bunny@gmail.com>
2024-06-09 15:57:21 +0530

    Adding s3 bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "testactionsfornode"
+    bucket = "nodeaction-01"
     key    = "terraform/terraform.tfstate"
     region = "us-east-1"
   }
